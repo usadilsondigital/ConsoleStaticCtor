c19ba337c7eb2b513b8b649aa9e8ce3b48baa6be
Adilson <usadilsondigital@gmail.com>
2024-01-31 10:29:22 -0600

    globalstarttime and routenumber in bus class

---
+++ Bus.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleStaticCtor
+{
+    public class Bus
+    {
+        //Static variable  used by all Bus Instances.
+        //Represent the time the first bus of the day starts its route.
+        public static readonly DateTime globalStartTime { get; set; }
+
+
+        //Property for the number of each bus.
+        protected int RouteNumber { get; set; }
+
+    }
+}
