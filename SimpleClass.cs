1ada716e109a9f1cf95238900d5c8d8f314a5783
Adilson <usadilsondigital@gmail.com>
2024-01-31 10:24:14 -0600

    initialized ctor simpleclass

--- SimpleClass.cs
+++ SimpleClass.cs
@@ -10,5 +10,12 @@ namespace ConsoleStaticCtor
     {
         //Static variable that must be initialized at run time.
         static readonly long baseline;
+
+        //Static ctor is called at most time, before any
+        //instance ctor is invoked or member is accessed.
+        static SimpleClass()
+        {
+            baseline = DateTime.Now.Ticks;
+        }
     }
 }
