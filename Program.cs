73ac829efc8e3eb7c26dbd41a20372961f1247e5
Adilson <usadilsondigital@gmail.com>
2024-02-01 12:49:44 -0600

    send the bus on its way

--- Program.cs
+++ Program.cs
@@ -6,6 +6,16 @@
         {
             // The creation of this instance activates the static constructor.
             Bus bus1 = new Bus(71);
+
+            // Create a second bus.
+            Bus bus2 = new Bus(72);
+
+
+            // Send bus1 on its way.
+            bus1.Drive();
+
+
+
         }
     }
 }
